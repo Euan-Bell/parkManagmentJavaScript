0a53e0234aa0730138a92037cb4395567572d77c
Euan <bell_designs@outlook.com>
2018-06-19 19:56:03 +0100

    running should have diet

--- specs/dinosaur_specs.js
+++ specs/dinosaur_specs.js
@@ -4,11 +4,20 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Dinosaur', function (){
 
-  it('should have a species', function (){
+  let dinosaur;
+
+  beforeEach(function () {
+  // arrange
       dinosaur = new Dinosaur('Trex', 'Carnivore', 500);
-    // // act
-    // const actualValue = taxi.manufacturer;
-    // // assert
+  })
+
+  it('should have a species', function (){
     assert.strictEqual(dinosaur.species, 'Trex');
   });
+
+  it('should have a diet', function (){
+    assert.strictEqual(dinosaur.diet, 'Carnivore');
+  });
+
+
 });
