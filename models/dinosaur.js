e722d1e007a538e3e5319ef9f5391d8cc038610d
Euan <bell_designs@outlook.com>
2018-06-19 19:48:45 +0100

    running have species test

---
+++ models/dinosaur.js
@@ -0,0 +1,7 @@
+const Dinosaur = function(species, diet, visitorsPerDay){
+  this.species = species;
+  this.diet = diet;
+  this.visitorsPerDay = visitorsPerDay;
+}
+
+module.exports = Dinosaur;
--- specs/dinosaur_specs.js
+++ specs/dinosaur_specs.js
@@ -1,2 +1,14 @@
 const assert = require('assert');
-const Taxi = require('../models/dinosaur.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+
+describe('Dinosaur', function (){
+
+  it('should have a species', function (){
+      dinosaur = new Dinosaur('Trex', 'Carnivore', 500);
+    // // act
+    // const actualValue = taxi.manufacturer;
+    // // assert
+    assert.strictEqual(dinosaur.species, 'Trex');
+  });
+});
